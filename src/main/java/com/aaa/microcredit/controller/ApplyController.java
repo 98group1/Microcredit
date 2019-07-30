c6abc52124bf2822919306bb9962c47eb06a7c60
QY98Group1 <1277367272@qq.com>
2019-07-30 16:04:21 +0800

    修改了apply表结构，添加了审核时间

--- src/main/java/com/aaa/microcredit/controller/ApplyController.java
+++ src/main/java/com/aaa/microcredit/controller/ApplyController.java
@@ -1,5 +1,6 @@
 package com.aaa.microcredit.controller;
 
+import com.aaa.microcredit.entity.Apply;
 import com.aaa.microcredit.service.ApplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
