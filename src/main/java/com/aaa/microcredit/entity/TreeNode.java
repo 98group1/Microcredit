1439ade791a4f347098d8353de10e57b80252e6e
SMITHLEI-Q <825363455@qq.com>
2019-08-02 11:15:43 +0800

    注册登录个人中心

---
+++ src/main/java/com/aaa/microcredit/entity/TreeNode.java
@@ -0,0 +1,11 @@
+package com.aaa.microcredit.entity;
+
+/**
+ * fileName:TreeNode
+ * description:
+ * author:雷建林
+ * ceeateTime:2019/7/31 11:27
+ */
+public class TreeNode {
+
+}
---
+++ src/main/java/com/aaa/microcredit/service/RegisterService.java
@@ -0,0 +1,42 @@
+package com.aaa.microcredit.service;
+
+import com.aaa.microcredit.entity.Login;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * fileName:RegisterService
+ * description:
+ * author:雷建林
+ * ceeateTime:2019/7/27 16:16
+ */
+public interface RegisterService {
+    /**
+     * 注册方法
+     * @param login
+     * @return
+     */
+    int addUser(Login login);
+
+    /**
+     * 登陆方法
+     * @param userName
+     * @return
+     */
+    List<Map> query1(String userName);
+
+    /**
+     * 查询注册时的用户名是否存在
+     * @param userName
+     * @return
+     */
+    String queryName(String userName);
+
+    /**
+     * 修改账户密码
+     * @param passWord
+     * @return
+     */
+    int changePwd(String passWord,Integer id);
+}
